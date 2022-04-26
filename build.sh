6ec6bbec987cb118c6c56f87d4d3e2b28a00076f
Coyault Peter <mimi@poutoux.fr>
2022-04-26 16:38:50 +0200

    edit build.sh to install dependency

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-#apt-get update
-#apt-get install -y live-build patch gnupg2 binutils zstd
-lb clean --purge
+apt-get update
+apt-get install -y live-build patch gnupg2 binutils zstd
+
 lb clean
 
 lb config 
-read
+
 lb build
