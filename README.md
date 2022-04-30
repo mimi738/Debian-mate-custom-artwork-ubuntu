2a2918b7d3746677a04502f7af51818f463fb877
Coyault Peter <mimi@poutoux.fr>
2022-04-30 10:38:11 +0200

    Add amd64 support

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 This is a system live debian mate. Included a themes of ubuntu artwork from https://github.com/VentGrey/debian-mate-themes, and add software to work nice for newbie user (vlc, simple-scan, gnome-software, thunderbird and firefox). There are non-free drivers for network connection.
 
-For the moment this system is available only in i386 it was thought to compensate for the fact that ubuntu does not maintain i386. But it can easily be used to build an amd64 system.
-
 # Build
 
 To build this debian live system use docker:
--- build.sh
+++ build.sh
@@ -1,11 +1,97 @@
 #!/bin/bash
 
 
+#Select arch
+
+if [ "$1" = "amd64" ]; then
+	arch="amd64"
+elif [ "$1" = "i386" ]; then
+	arch="i386"
+else
+    echo "1. amd64"
+    echo "2. i386"
+    read -r -p "Select your system architecture: " answer
+    case $answer in
+        [1]* ) arch="amd64";;
+        [2]* ) arch="i386";;
+        * ) echo "Not arch selected !"; exit 1;;
+    esac	
+fi
+
+
+mkdir auto &> /dev/null
+
+if [ "$arch" = "amd64" ]; then
+    sed -i -e "s/linux64/linux/g" config/hooks/normal/0001-firefox-latest-install.hook.chroot
+    sed -i -e "s/linux/linux64/g" config/hooks/normal/0001-firefox-latest-install.hook.chroot
+    cat <<'EOF' >auto/config
+#!/bin/sh
+
+lb config noauto \
+	--clean \
+	--architectures amd64 \
+	--mode debian \
+	--distribution  bullseye \
+	--initramfs live-boot \
+	--archive-areas "main contrib non-free" \
+	--debian-installer live \
+	--linux-flavours "amd64" \
+	--linux-packages "linux-image" \
+	--source "false" \
+"${@}"   
+EOF
+
+
+elif [ "$arch" = "i386" ]; then
+    sed -i -e "s/linux64/linux/g" config/hooks/normal/0001-firefox-latest-install.hook.chroot
+
+    cat <<'EOF' >auto/config
+#!/bin/sh
+
+lb config noauto \
+	--clean \
+	--architectures i386 \
+	--mode debian \
+	--distribution  bullseye \
+	--initramfs live-boot \
+	--archive-areas "main contrib non-free" \
+	--debian-installer live \
+	--linux-flavours "686 686-pae" \
+	--linux-packages "linux-image" \
+	--source "false" \
+"${@}"   
+EOF
+
+fi
+
+
+echo "You are going to build Debian mate live sytem in $arch."
+if [ "$2" != "noquestion" ]; then
+    read -n1 -p "Press [ENTER] to start ...." KEY
+    if [[ "$KEY" != "" ]]
+    then
+        exit 1;
+    fi
+fi
+
+#Start Build System
+
+echo "Installing dependencies to build system"
 apt-get update
 apt-get install -y live-build patch gnupg2 binutils zstd
 
+echo "Clean directory"
 lb clean
 
-lb config 
 
+echo "Build System"
+lb config 
 lb build
+
+
+
+
+
+
+
+
